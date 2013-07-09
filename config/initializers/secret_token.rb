88613cbe0f8b5af445baae860a278dcc2a3c4222
qinyan <tangl@tangl>
2013-07-09 17:48:31 +0800

    new file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qinyan::Application.config.secret_token = '499c9db46ed4b3e4b91d444308822ab132978c9e9c34b5af3b3e0cc413fe03f68d9b64c363eb89070c0d23fc6c3819510cba853aa02476a819642c7f4d12d351'
