84095c14fb6d88a1d93fc4df58e6ddd01a20b7c7
qinyan <tangl@tangl>
2013-11-15 17:18:58 +0800

    delegate

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,6 +4,7 @@ class Blog < ActiveRecord::Base
   # acts_as_global_primary_key
   belongs_to :user
   before_save :add_intro
+  delegate :email, :to => :user
 
   def add_intro
     self.intro = self.content.gsub(/&nbsp;/, '').gsub(/<.*?>/, '').truncate(210, :omission=>'...') if self.content.present?
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ActiveRecord::Base
   has_paper_trail
   mount_uploader :logo, UserLogoUploader
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-  delegate :intro, :to => :blog
   
   def cropping?
     !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
